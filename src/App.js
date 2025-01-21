653a611ad70c27f909b33e1e938c688deb8bdecf
Hisham alkhellow <hishamalkhellow@gmail.com>
2025-01-21 14:46:42 +0300

    back to local api

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://smart-brain-api-2mum.onrender.com/image', {
+          fetch('http://localhost:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -30,7 +30,7 @@ class Register extends React.Component {
 
 
   onSubmitSignIn = () => {
-    fetch('https://smart-brain-api-2mum.onrender.com/register', {
+    fetch('http://localhost:3000/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
