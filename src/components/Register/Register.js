0ffbd2189db12ba5cd74358e6842b09c284cd659
Hisham alkhellow <hishamalkhellow@gmail.com>
2025-01-21 12:46:21 +0300

    update server

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -30,7 +30,7 @@ class Register extends React.Component {
 
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://smart-brain-api-2mum.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
